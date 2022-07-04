e8e0a0d28df804323c283937d2091c2842f25cd4
FabTrindade <fabrtrindade@gmail.com>
2022-07-03 21:50:54 -0300

    Nesting tests.

--- src/App.test.js
+++ src/App.test.js
@@ -4,9 +4,21 @@ import {render, screen} from '@testing-library/react'
 import App from './app'
 
 describe('Componente principal', () => {
-    it ('Mostrar o nome do banco', () => {
-        render (<App />);
-
-        expect(screen.getByText('ByteBank').toBeInTheDocument);
-    })
+    describe('Ao abrir aplicativo...', () => {
+        it ('mostrar o nome do banco', () => {
+            render (<App />);
+    
+            expect(screen.getByText('ByteBank').toBeInTheDocument);
+        })
+        it ('mostrar saldo', () => {
+            render (<App />);
+    
+            expect(screen.getByText('Saldo:').toBeInTheDocument);
+        })
+        it ('mostrar botão de realizar operação', () => {
+            render (<App />);
+    
+            expect(screen.getByText('Realizar operação').toBeInTheDocument);
+        })
+    })    
 })
